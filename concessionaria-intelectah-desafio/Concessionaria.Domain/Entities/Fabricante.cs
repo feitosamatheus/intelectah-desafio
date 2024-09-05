bc7580c2b7972c12faaac1c7c038f37b76041343
feitosamatheus <feitosamatheus21@hotmail.com>
2024-09-05 00:45:35 -0300

    fix: alterando tipo do ano de fabriacao de datetime para int

--- concessionaria-intelectah-desafio/Concessionaria.Domain/Entities/Fabricante.cs
+++ concessionaria-intelectah-desafio/Concessionaria.Domain/Entities/Fabricante.cs
@@ -9,8 +9,15 @@ namespace Concessionaria.Domain.Entities
     public class Fabricante
     {
         public int FabricanteId { get; private set;}
-        public string Nome { get; private  set; } // deve ser único
-        public DateTime AnoFundacao { get; private  set; }
+        public string Nome { get; private set; } // deve ser único
+        public int AnoFundacao { get; private  set; }
         public string WebSite { get; private set; }
+
+        public Fabricante(string nome, int anoFundacao, string webSite)
+        {
+            Nome = nome;
+            AnoFundacao = anoFundacao;
+            WebSite = webSite;
+        }
     }
 }
