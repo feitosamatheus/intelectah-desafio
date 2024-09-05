db618a0d88df8a90da8dd0e31c5c2c08ce8afb9b
feitosamatheus <feitosamatheus21@hotmail.com>
2024-09-05 02:20:38 -0300

    feat: implementando contexto

---
+++ concessionaria-intelectah-desafio/Concessionaria.Infrastructure/Contexts/ContextApp.cs
@@ -0,0 +1,27 @@
+﻿using ConcessionariaApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConcessionariaApp.Infrastructure.Contexts
+{
+    public class ContextApp : DbContext
+    {
+        public ContextApp(DbContextOptions<ContextApp> options) : base(options){ }
+
+        public virtual DbSet<Usuario> Usuarios { get; set; }
+        public virtual DbSet<Cliente> Clientes { get; set; }
+        public virtual DbSet<Concessionaria> Concessionarias { get; set; }
+        public virtual DbSet<Fabricante> Fabricantes { get; set; }
+        public virtual DbSet<Veiculo> Veiculos { get; set; }
+        public virtual DbSet<Venda> Vendas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(typeof(ContextApp).Assembly);
+        }
+    }
+}
