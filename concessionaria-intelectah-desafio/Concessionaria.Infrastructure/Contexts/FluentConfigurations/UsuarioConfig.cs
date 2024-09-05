553ce5ecb7abce21d01e29415c20dc6b2d112850
feitosamatheus <feitosamatheus21@hotmail.com>
2024-09-05 02:21:19 -0300

    feat: criando pasta FluentConfigurations e implementando configuração do usuario

---
+++ concessionaria-intelectah-desafio/Concessionaria.Infrastructure/Contexts/FluentConfigurations/UsuarioConfig.cs
@@ -0,0 +1,23 @@
+﻿using ConcessionariaApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConcessionariaApp.Infrastructure.Contexts.FluentConfigurations
+{
+    public class UsuarioConfig : IEntityTypeConfiguration<Usuario>
+    {
+        public void Configure(EntityTypeBuilder<Usuario> builder)
+        {
+            builder.HasKey(t => t.UsuarioId);
+            builder.Property(p => p.NomeUsuario).HasMaxLength(50).IsRequired();
+            builder.Property(p => p.Senha).HasMaxLength(255).IsRequired();
+            builder.Property(p => p.Email).HasMaxLength(100).IsRequired();
+            builder.Property(p => p.NivelAcesso).IsRequired();
+        }
+    }
+}
