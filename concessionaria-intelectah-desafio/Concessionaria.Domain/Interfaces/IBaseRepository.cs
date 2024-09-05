aed4078def2a67537d1d7baa9417caf1725be64e
feitosamatheus <feitosamatheus21@hotmail.com>
2024-09-05 01:05:33 -0300

    feat: implementando interfaces dos repositories na camanda de domain

---
+++ concessionaria-intelectah-desafio/Concessionaria.Domain/Interfaces/IBaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concessionaria.Domain.Interfaces
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        void Add(T value);
+        void Delete(T value);
+        void Update(T value);
+        Task<T> GetAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+    }
+}
---
+++ concessionaria-intelectah-desafio/Concessionaria.Domain/Interfaces/IConcessionariaRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concessionaria.Domain.Interfaces
+{
+    public interface IConcessionariaRepository 
+    {
+    }
+}
