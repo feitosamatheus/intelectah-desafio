ab6fb27d928372b1592a6bddf93b747cbc8a3e27
feitosamatheus <feitosamatheus21@hotmail.com>
2024-09-06 15:10:20 -0300

    build: implementando factory para realização de migrations

---
+++ concessionaria-intelectah-desafio/ConcessionariaApp.Infrastructure/Contexts/ContextAppFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace ConcessionariaApp.Infrastructure.Contexts
+{
+    public class ContextAppFactory : IDesignTimeDbContextFactory<ContextApp>
+    {
+        public ContextApp CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ContextApp>();
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-6F7Q82U\\SQLEXPRESS;Initial Catalog=ConcessionariaDatabase;Integrated Security=True; TrustServerCertificate=True;");
+
+            return new ContextApp(optionsBuilder.Options);
+        }
+    }
+}
