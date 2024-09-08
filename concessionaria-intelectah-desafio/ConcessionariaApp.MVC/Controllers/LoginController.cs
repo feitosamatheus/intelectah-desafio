b37a63613e5810642b8179d105dd3608c8d672ec
feitosamatheus <feitosamatheus21@hotmail.com>
2024-09-08 04:26:00 -0300

    feat: implementando paginas de login de usuario

---
+++ concessionaria-intelectah-desafio/ConcessionariaApp.MVC/Controllers/LoginController.cs
@@ -0,0 +1,57 @@
+﻿using ConcessionariaApp.Application.Dtos.Autenticacao;
+using ConcessionariaApp.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConcessionariaApp.MVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly ILoginService _loginService;
+
+        public LoginController(ILoginService loginService)
+        {
+            _loginService = loginService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Index(AutenticarUsuarioDTO dto)
+        {
+            if (!ModelState.IsValid)
+                return View(dto);
+            
+            var usuarioResultado = await _loginService.Login(dto);
+
+            return usuarioResultado.Sucesso ? 
+                        RedirectToAction("Index", "Home") : View(usuarioResultado);
+        }
+        
+        public IActionResult Registrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Registrar(RegistrarUsuarioDTO dto)
+        {
+            if (!ModelState.IsValid)
+                return View(dto);
+
+            var usuarioResultado = await _loginService.Registrar(dto);
+
+            return usuarioResultado.Sucesso ?
+                        RedirectToAction("Index", "Home") : View(usuarioResultado);
+        }
+
+        public IActionResult Logout()
+        {
+            return View();
+        }
+    }
+}
