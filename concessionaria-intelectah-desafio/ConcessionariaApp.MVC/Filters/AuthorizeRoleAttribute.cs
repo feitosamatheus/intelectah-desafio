f4e26e001196f0f39e51126411264326f5d85452
feitosamatheus <feitosamatheus21@hotmail.com>
2024-09-08 04:14:59 -0300

    feat: criando atributo para validação de roles

---
+++ concessionaria-intelectah-desafio/ConcessionariaApp.MVC/Filters/AuthorizeRoleAttribute.cs
@@ -0,0 +1,15 @@
+﻿using ConcessionariaApp.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ConcessionariaApp.MVC.Filters
+{
+    public class AuthorizeRoleAttribute : TypeFilterAttribute
+    {
+        public AuthorizeRoleAttribute(string role) : base(typeof(AuthorizeRoleFilter))
+        {
+            Arguments = new object[] { role };
+        }
+    }
+}
---
+++ concessionaria-intelectah-desafio/ConcessionariaApp.MVC/Filters/AuthorizeRoleFilter.cs
@@ -0,0 +1,29 @@
+﻿using ConcessionariaApp.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConcessionariaApp.MVC.Filters
+{
+    public class AuthorizeRoleFilter
+    {
+        private readonly string _role;
+        private readonly UserManager<Usuario> _userManager;
+
+        public AuthorizeRoleFilter(string role, UserManager<Usuario> userManager)
+        {
+            _role = role;
+            _userManager = userManager;
+        }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var user = _userManager.GetUserAsync(context.HttpContext.User).Result;
+
+            if (user == null || user.NivelAcesso.ToString() != _role)
+            {
+                context.Result = new RedirectToActionResult("AccessDenied", "Account", null);
+            }
+        }
+    }
+}
